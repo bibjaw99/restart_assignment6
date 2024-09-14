b449ea22586bec03a22e199f7b7b54c449663c60
Ahsan Habib <bibjaw99@gmail.com>
2024-09-14 11:16:29 +0600

    Discussion Section started

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      backgroundImage: {
+        banner: "url('./src/bgbanner.png')",
+      },
+    },
+  },
+  plugins: [],
+  daisyui: {
+    themes: ["light"],
+  },
+};
